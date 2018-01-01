08489f433cf389ce915035d8b6c04e25ae6afde2
Sousa-L <leonardo.s.sousa1945@gmail.com>
2018-01-01 18:29:10 -0300

    Tela de Welcome com layout pronto

--- App.js
+++ App.js
@@ -3,31 +3,11 @@ import { Platform, StatusBar } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 import WelcomeScreen from './src/pages/Welcome';
-import SelectScreen from './src/pages/Select';
-import MapScreen from './src/pages/Map';
-import ComputerMapScreen from './src/pages/Computermap';
-import AirconditionerMapScreen from './src/pages/Airconditionermap';
-import PrinterMapScreen from './src/pages/Printermap';
-import RefrigeratorMapScreen from './src/pages/Refrigeratormap';
+
 
 const Main = StackNavigator({
   Home: {
     screen: WelcomeScreen,
-  },
-  Select: {
-    screen: SelectScreen,
-  },
-  ComputerMap: {
-    screen: ComputerMapScreen,
-  },
-  AirconditionerMap: {
-    screen: AirconditionerMapScreen,
-  },
-  PrinterMap: {
-    screen: PrinterMapScreen,
-  },
-  RefrigeratorMap: {
-    screen: RefrigeratorMapScreen,
   },
 });
 
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
+import { Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Content } from 'native-base';
 
-
+import { Logo, Background } from '../../components';
+import styles from './styles';
+import {metrics, fonts, colors} from '../../styles';
 
 export default class WelcomeScreen extends React.Component {
     static navigationOptions = {
@@ -10,58 +13,30 @@ export default class WelcomeScreen extends React.Component {
     render() {
       const {navigate} = this.props.navigation;
       return (
-        <View style={styles.container}>
-          <Image style={styles.background} source={require('../img/background.jpg')} />
-          <Image style={styles.background2} source={require('../img/pattern.png')} />
-          <Image style={styles.icon} source={require('../img/icont.png')} />
-            <TouchableOpacity style={styles.button} onPress={() => navigate('Select')}>
-              <Text style={styles.textLight}>Clique para começar</Text>
-            </TouchableOpacity>
-        </View>
+        <Container>
+          <Content showsVerticalScrollIndicator={false}>
+            <View style={styles.container}>
+              <Background image={require('../../images/background.jpg')} />
+              <View style={styles.content}>
+                <Logo size={200} />
+                <Text style={{margin:metrics.baseMargin, fontSize: fonts.bigger, fontWeight: 'bold', color: colors.primary}}>Deu Problema? iHelp</Text>
+              </View>
+              <View style={{ marginTop: '11%'}}>
+                <Text style={{alignSelf: 'center', margin:metrics.baseMargin, fontSize: fonts.big, fontWeight: 'bold', color: colors.primary}}>Crie uma conta</Text>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Text style={{margin:metrics.doubleBaseMargin, fontSize: fonts.big, color: colors.primary}}>Email:</Text>
+                  <TouchableOpacity style={{alignItems: 'center', justifyContent: 'center', borderRadius: 10, width: '69%', height: 40, backgroundColor: 'rgba(255,255,255,0.6)'}}>
+                    <Text style={{margin:metrics.baseMargin, fontSize: fonts.regular, color: colors.primary}}>Clique aqui para digitar seu email</Text>
+                  </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={{margin:metrics.baseMargin, alignItems: 'center'}}>
+                  <Text style={{fontSize: fonts.big, fontWeight: 'bold', color: colors.primary}}>Clique aqui</Text>
+                  <Text style={{fontSize: fonts.big, fontWeight: 'bold', color: colors.primary}}>se já tiver uma conta</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Content>
+        </Container>
       );
     }
   }
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#d4efe3',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    icon: {
-      marginBottom: 243,
-      height: 200,
-      width: 200,
-    },
-    background: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
-    },
-    background2: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
-      opacity: 0.4,
-    },
-    textLight: {
-      color: '#fff',
-    },
-    textDark: {
-        color: '#fff',
-    },
-    button: {
-      width: '75%',
-      backgroundColor: 'rgba(80, 149, 240, 0.6)',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingTop: 7,
-      paddingBottom: 7,
-      borderRadius: 10,
-    },
-  });
