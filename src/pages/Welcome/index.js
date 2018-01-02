a40d2d45a6e715e952183c6b2db579ab698640d2
Sousa-L <leonardo.s.sousa1945@gmail.com>
2018-01-01 23:07:33 -0300

    Mudanças na pagina de Welcome

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
-import { Container, Header, Left, Body, Right, Button, Icon, Title, Content } from 'native-base';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Content, Item, Input, Label } from 'native-base';
 
 import { Logo, Background } from '../../components';
 import styles from './styles';
@@ -23,14 +23,14 @@ export default class WelcomeScreen extends React.Component {
               <View style={{ bottom: 0, backgroundColor: colors.primary}}>
                 <Text style={{alignSelf: 'center', margin:metrics.baseMargin, fontSize: fonts.big, fontWeight: 'bold', color: colors.white}}>Crie uma conta</Text>
                 <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                  <Text style={{margin:metrics.doubleBaseMargin, fontSize: fonts.big, color: colors.white}}>Email:</Text>
-                  <TouchableOpacity style={{alignItems: 'center', justifyContent: 'center', borderRadius: 10, width: '69%', height: 40, backgroundColor: colors.primaryDarker}}>
-                    <Text style={{margin:metrics.baseMargin, fontSize: fonts.regular, color: colors.white}}>Clique aqui para digitar seu email</Text>
-                  </TouchableOpacity>
+                  <Item style={{borderColor: '#fff'}}>
+                    <Label style={{margin:metrics.baseMargin, fontSize: fonts.big, fontWeight: 'bold', color: colors.white}}>Email:</Label>
+                    <Input placeholderTextColor='#c0c0c0' placeholder="Clique aqui para digitar seu email" />
+                  </Item>
                 </View>
                 <TouchableOpacity style={{margin:metrics.doubleBaseMargin, alignItems: 'center'}}>
                   <Text style={{fontSize: fonts.big, fontWeight: 'bold', color: colors.white}}>Clique aqui</Text>
-                  <Text style={{fontSize: fonts.big, fontWeight: 'bold', color: colors.white}}>se já tiver uma conta</Text>
+                  <Text style={{fontSize: fonts.big, fontWeight: 'bold', color: colors.white}}>se já tiver uma conta :)</Text>
                 </TouchableOpacity>
               </View>
             </View>
